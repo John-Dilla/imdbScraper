3bd94b65c41109a5cf81af839fb6a231ee7898dc
John-Dilla <yungdillaa@gmail.com>
2021-05-12 19:51:53 +0200

    wip

--- src/ui/mainWindow.py
+++ src/ui/mainWindow.py
@@ -3,7 +3,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtCore import Qt
 
 
-qt_creator_file = "mainwindow.ui"
+qt_creator_file = "mainWindow.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qt_creator_file)
 
 
