e1a15e9cd12a35ca3db23fe9ae63b7e2bdd137e8
John-Dilla <yungdillaa@gmail.com>
2021-05-25 17:48:53 +0200

    Implemented picture support

--- src/utility/fileHandler.py
+++ src/utility/fileHandler.py
@@ -1,14 +1,16 @@
+from os.path import dirname, join
 import os
+
 import pandas as pd
 import urllib.request
 
-rootPath = os.path.join(os.getcwd(), 'database')
+rootPath = join(os.getcwd(), 'database')
 
 def createDatabase():
-    filmography = os.path.join(rootPath, 'filmography')
-    biography = os.path.join(rootPath, 'biography')
-    awards = os.path.join(rootPath, 'awards')
-    genre = os.path.join(rootPath, 'genre')
+    filmography = join(rootPath, 'filmography')
+    biography = join(rootPath, 'biography')
+    awards = join(rootPath, 'awards')
+    genres = join(rootPath, 'genres')
 
     try:
         os.makedirs(filmography)
@@ -23,13 +25,13 @@ def createDatabase():
     except OSError:
         pass
     try:
-        os.makedirs(genre)
+        os.makedirs(genres)
     except OSError:
         pass
 
 def writeToDirectory(folder: str, fileName: str, dataframe) -> None: 
-    print("Saving File:", os.path.join(rootPath, folder, fileName)+".csv")
-    p = os.path.join(rootPath, folder, fileName)+".csv"
+    print("Saving File:", join(rootPath, folder, fileName)+".csv")
+    p = join(rootPath, folder, fileName)+".csv"
 
     # Exception can happen when executed for the very first time. 
     # If so, no database is found.
@@ -40,12 +42,13 @@ def writeToDirectory(folder: str, fileName: str, dataframe) -> None:
         dataframe.to_csv(p, mode='w', sep=';')
 
 def writeProfilepicture(fileName: str, imageURL: str) -> None:
-    picturePath = os.path.join(rootPath, "biography", fileName)+".jpg"
+    picturePath = join(rootPath, "biography", fileName)+".jpg"
     urllib.request.urlretrieve(imageURL, picturePath)
+    print("Saving File:", picturePath)
 
 
 def getTable(folder: str, fileName: str):
-    filePath = os.path.join(rootPath, folder, fileName)+".csv"
+    filePath = join(rootPath, folder, fileName)+".csv"
     dataframe = pd.read_csv(filePath, sep=';')
     if 'Year' in dataframe.columns:
         dataframe['Year'] = dataframe['Year'].astype('Int64')
@@ -55,7 +58,11 @@ def getTable(folder: str, fileName: str):
         dataframe['Ranking'] = dataframe['Ranking'].astype('Int64')
     return dataframe
 
-def getUIPath(window: str):
-    ui_path = os.path.dirname(os.path.abspath(__file__))
-    pathUI = os.path.join(ui_path, "ui", window)
+def getUIPath(absolutePath, uiFile: str):
+    ui_path = dirname(absolutePath)
+    pathUI = join(ui_path, "ui", uiFile)
+    return pathUI
+
+def getPicture(id):
+    pathUI = join(rootPath, "biography", id) + ".jpg"
     return pathUI
