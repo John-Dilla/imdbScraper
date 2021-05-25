79ef7037ba63d2972c45eff5bb01b24a5f69fa63
John-Dilla <yungdillaa@gmail.com>
2021-05-25 17:49:20 +0200

    Added functionality to actor window

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ from src.controller.mainWindow import MainWindow
 
 import src.utility.fileHandler as io
 
+from src.scraper.scrapeController import Controller
+
 def main():    
     #Create and structure database if it does not exist
     io.createDatabase()
 
     app = QtWidgets.QApplication(sys.argv)
     mainUI = MainWindow()
+    mainUI.show()
     app.exec_() 
+    #test = Controller("https://www.imdb.com/list/ls053501318/")
+    #test.test()
+
 
 if __name__ == "__main__":
     main()
